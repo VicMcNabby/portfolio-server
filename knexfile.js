ad8679a5c786187224ca44ca81c9bef312fddde4
Matthew Victor <vicmcnabby@gmail.com>
2018-03-19 16:04:13 -0600

    added heroku

--- knexfile.js
+++ knexfile.js
@@ -5,5 +5,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/portfolio'
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
   }
 };
